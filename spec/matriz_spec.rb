86f13f7746171c61ea0c14cec171920411e8119a
BALLESTER HERNANDEZ <alu4371@cc424.(none)>
2013-11-05 15:48:15 +0000

    Pruebas constructor

--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -1,1 +1,22 @@
-# Implementar en este fichero las Pruebas Unitarias asociadas a la clase Matriz
+require "matriz.rb"
+
+
+describe Matriz
+
+	describe "Pruebas Constructor \n" do
+		  
+			it "Debe estar inicializado" do
+				@a=Matriz.new(2,2)
+				@a.to_s.should eq("[0 0] \n[0 0] \n")  
+			 end
+
+			it "Se le debe poder asignar un valor predeterminado" do
+				@a=Matriz.new(2,2,1)
+				@a.to_s.should eq("[1 1] \n[1 1] \n")  
+			end
+
+			it "Se le debe poder asignar valores por array" do
+				@a=Matriz.new(2,2,[1,2,3,4])
+				@a.to_s.should eq("[1 2] \n[3 4] \n")  
+			end
+	end
---
+++ spec/matriz_spec.rb~
@@ -0,0 +1,1 @@
+# Implementar en este fichero las Pruebas Unitarias asociadas a la clase Matriz
